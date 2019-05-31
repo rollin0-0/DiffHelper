d00ffe04c4939822d4bedccb93529762bd03be5a
Rowling <rowliny@gmail.com>
2019-05-31 09:56:26 +0800

    *）修复安装依赖的脚本

--- INSTALL.py
+++ INSTALL.py
@@ -5,16 +5,13 @@ import os
 
 
 
-try:
-	pipVersion =  ""
-	if sys.version_info[0] > 3:
-		pipVersion = "pip3"
-		import pip3
-		print('检测到已经安装pip模块')
-except:
-	print('检测到未安装pip模块,现在开始安装......')
-	command = "sudo easy_install pip3"
-	os.system(command)
+# try:
+# 	if sys.version_info[0] >= 3:
+# 		import pip3
+# 		print('检测到已经安装pip模块')
+# except:
+# 	print('检测到未安装pip模块,现在开始安装......')
+# 	os.system("sudo easy_install pip3")
 	
 
 try:
@@ -22,7 +19,7 @@ try:
 	print('检测到已经安装nltk模块')
 except:
 	print('检测到未安装nltk模块,现在开始安装......')
-	command = pipVersion + " install nltk"
+	command = "pip3 install nltk"
 	os.system(command)
 
 try:
@@ -30,7 +27,7 @@ try:
 	print('检测到已经安装ntplib模块')
 except:
 	print('检测到未安装ntplib模块,现在开始安装......')
-	command = pipVersion + " install ntplib"
+	command = "pip3 install ntplib"
 	os.system(command)
 
 try:
@@ -38,16 +35,14 @@ try:
 	print('检测到已经安装requests模块')
 except:
 	print('检测到未安装requests模块,现在开始安装......')
-	command = pipVersion + " install requests"
-	os.system(command)
+	os.system("pip3 install requests")
 
 try:
 	import PyYAML
 	print('检测到已经安装PyYAML模块')
 except:
 	print('检测到未安装PyYAML模块,现在开始安装......')
-	command = pipVersion + " install PyYAML"
-	os.system(command)
+	os.system("pip3 install PyYAML")
 
 print("-------------------------------------依赖环境安装完成-------------------------------------")
 print("请切换到路径:iOSCodeDifferHelper \n执行python3 start.py即可开始")
