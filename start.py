efedd4d52b1f21a9987b9c58f6de282ea6b46737
Rowling <rowliny@gmail.com>
2019-09-27 13:43:48 +0800

    更新启动脚本

--- start.py
+++ start.py
@@ -3,21 +3,12 @@
 import os
 import sys
 import platform
+from distutils.version import StrictVersion
 
 # 判断当前运行操作系统
 if platform.system() != "Darwin":
     print("请在Mac OS操作系统运行该工具")
     os._exit(1)
-
-# 判断当前运行python环境
-if sys.version_info[0] < 3:
-    print("请确认在python3环境运行该工具")
-    os._exit(1)
-
-# 判断是否安装xcode
-# if not os.path.exists("/Applications/Xcode.app"):
-#     print("安装xcode后才能正常运行")
-#     os._exit(1)
 
 # 路径空格判断
 if " " in os.getcwd():
@@ -28,7 +19,7 @@ if " " in os.getcwd():
 # 需要安装的依赖以及对应的版本
 kRequiredLib = {
     "pbxproj": "2.5.1",
-    "nltk": "3.4.1",
+    "nltk": "3.4.5",
     "ntplib": "0.3.3",
     "requests": "2.21.0",
     "PyYAML": "5.1.2",
@@ -39,16 +30,14 @@ kRequiredLib = {
 
 # 判断当前是否存在pip3
 def pip3Check():
-    command = "pip3 list"
-    output = os.popen(command)
+    output = os.popen("pip3 list")
     lines = output.readlines()
     output.close()
     if len(lines) == 0:
         print('检测到未安装pip3模块,现在开始安装......')
         os.system("sudo easy_install pip3")
         return None
-    else:
-        return lines
+    return lines
 
 
 # 获取已经安装的依赖库
@@ -68,16 +57,16 @@ def getInstalledLib():
 
 # 安装依赖库
 def installLib(lib, requiredVersion):
-    command = "pip3 install %s==%s" % (lib, requiredVersion)
-    os.system(command)
+    os.system("pip3 install %s==%s" % (lib, requiredVersion))
 
 
 def operateEnvOK():
     installedDict = getInstalledLib()
     for lib in kRequiredLib:
+        curVersion = installedDict[lib]
         requiredVersion = kRequiredLib[lib]
         if lib in installedDict:
-            if installedDict[lib] != requiredVersion:
+            if StrictVersion(curVersion) < StrictVersion(requiredVersion):
                 installLib(lib, requiredVersion)
         else:
             installLib(lib, requiredVersion)
@@ -87,22 +76,54 @@ def operateEnvOK():
     for lib in kRequiredLib:
         if lib not in installedDict:
             allInstalled = False
-
     return allInstalled
 
 
+# 判断当前运行python环境
+def curPythonVersion():
+    print("Python: %s" % (platform.python_version()))
+    if sys.version_info[0] < 3:
+        print("请确认在python3环境运行该工具")
+        print("或者安装工具文件夹下的python-3.7.3-macosx10.9.pkg")
+        os._exit(1)
+
+
+# 当前xcode版本信息
+def checkXcodeInstalled():
+    output = os.popen("/usr/bin/xcodebuild -version")
+    lines = output.readlines()
+    output.close()
+    for line in lines:
+        print(line.strip())
+
+
+# 判断是否安装xcode-select
+def xcodeSelect():
+    output = os.popen(
+        "if xcode-select -p &>/dev/null; then echo 'installed'; else echo 'notInstall'; fi"
+    )
+    content = output.readline()
+    output.close()
+    if content == "notInstall":
+        print("当前未安装xcode-select,现在开始安装:")
+        os.system("xcode-select --install")
+
+
 def checkOperateEnv():
-    print("开始检测运行环境.")
+    print("*" * 32 + "开始检测运行环境" + "*" * 32)
+    curPythonVersion()
+    checkXcodeInstalled()
+    xcodeSelect()
     time = 1
     while not operateEnvOK():
         if time > 3:
             print("多次安装依赖库失败,请确认网络连接,重启终端重试")
             os._exit(1)
         else:
-            print("\n运行环境不符合要求,正在安装工具依赖[%d]" % (time))
+            print("\n运行环境不符合要求,正在安装依赖[%d]" % (time))
             operateEnvOK()
             time += 1
-    print("-------------------------运行环境符合要求-------------------------")
+    print("*" * 32 + "运行环境符合要求" + "*" * 32)
 
 
 checkOperateEnv()
