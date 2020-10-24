2dbac4d3f4937c209e7977f839d2c9736db3a73c
iOSCoda <rowliny@gmail.com>
2020-10-24 17:51:46 +0800

    更新启动脚本

--- start.py
+++ start.py
@@ -7,19 +7,24 @@ import sys
 from distutils.version import StrictVersion
 
 
-def colorPrint(log, color="31"):
+Red = "31"
+Green = "32"
+Yellow = "33"
+
+
+def colorPrint(color, log):
     print("\033[0;%s;m%s\033[0m" % (color, log))
 
 
 # 判断当前运行操作系统
 if platform.system() != "Darwin":
-    colorPrint("请在Mac OS操作系统运行该工具")
+    colorPrint(Red, "请在Mac OS操作系统运行该工具")
     os._exit(1)
 
 parPath = os.path.dirname(os.path.abspath(__file__))
 if " " in parPath:
-    colorPrint(parPath)
-    colorPrint("工具所在的路径包含了空格,请移除路径中的空格后重试")
+    colorPrint(Red, parPath)
+    colorPrint(Red, "工具所在的路径包含了空格,请移除路径中的空格后重试")
     os._exit(1)
 
 # 需要安装的依赖以及对应的版本
@@ -40,38 +45,39 @@ def curSystemVersion():
     lines = output.readlines()
     output.close()
     systemVersion = lines[0].strip()
-    colorPrint("MacOS: " + systemVersion, "32")
+    colorPrint(Green, "MacOS: " + systemVersion)
     if StrictVersion(systemVersion) >= StrictVersion("10.15"):
         output = os.popen("spctl --status | awk 'NR==1 {print $2}'")
         status = output.readline()
         output.close()
         if status.strip() == "disabled":
             return
-        colorPrint("10.15系统开始,使用本工具需要关闭spctl", "33")
-        colorPrint("请在终端执行以下命令来关闭spctl:", "33")
-        print("sudo spctl --master-disable")
-        colorPrint("因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
-        colorPrint('执行完成后需要设置: 系统偏好设置 -> 安全性与隐私 -> 通用 -> 选择"任何来源"')
-        colorPrint("设置之后重新运行当前脚本即可")
+        colorPrint(Yellow, "10.15系统开始,使用本工具需要关闭spctl")
+        colorPrint(Yellow, "请在终端执行以下命令来关闭spctl:")
+        colorPrint(Green, "sudo spctl --master-disable")
+        colorPrint(Red, "因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
+        colorPrint(Red, '执行完成后需要设置: 系统偏好设置.app -> 安全性与隐私 -> 通用 -> 选择"任何来源"')
+        colorPrint(Red, "设置之后重新运行当前脚本即可")
         os._exit(1)
 
 
 # 判断当前运行python环境
 def curPythonVersion():
-    colorPrint("Python: " + platform.python_version(), "32")
+    colorPrint(Green, "Python: " + platform.python_version())
     if platform.python_version() != "3.7.3":
-        colorPrint("请确认在python3.7.3环境运行该工具,其他版本确认不能正常运行")
-        colorPrint("详情见:", "32")
-        colorPrint("https://github.com/iOSCoda/DiffHelper/wiki/Python环境安装", "33")
+        colorPrint(Red, "请确认在python3.7.3环境运行该工具,其他版本确定不能正常运行")
+        colorPrint(Green, "详情见:")
+        colorPrint(Yellow, "https://github.com/iOSCoda/DiffHelper/wiki/Python环境安装")
+        colorPrint(Yellow, "或者直接安装附带的python安装包:python-3.7.3-macosx10.9.pkg")
         os._exit(1)
 
 
 def checkPipTip():
-    colorPrint("本工具依赖pip3,检测到未安装pip3模块", "33")
-    colorPrint("请在终端执行以下命令来安装pip3:", "33")
-    print("sudo easy_install pip3")
-    colorPrint("因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
-    colorPrint("执行完成后重新运行当前脚本即可")
+    colorPrint(Yellow, "本工具依赖pip3,检测到未安装pip3模块")
+    colorPrint(Yellow, "请在终端执行以下命令来安装pip3:")
+    colorPrint(Green, "sudo easy_install pip3")
+    colorPrint(Red, "因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
+    colorPrint(Red, "执行完成后重新运行当前脚本即可")
     os._exit(1)
 
 
@@ -90,7 +96,7 @@ def curPip():
         else:
             curPipViersion = resultList[0]
             if curPipViersion == "python 3.7":
-                colorPrint("Pip3: " + curPipViersion, "32")
+                colorPrint(Green, "Pip3: " + curPipViersion)
             else:
                 checkPipTip()
 
@@ -103,7 +109,7 @@ def checkXcodeInstalled():
     if not lines:
         colorPrint("请确认已经安装Xcode")
         os._exit(1)
-    colorPrint(lines[0].strip(), "32")
+    colorPrint(Green, lines[0].strip())
 
 
 # 判断是否安装xcode-select
@@ -112,14 +118,14 @@ def checkXcodeSelect():
     content = output.readline()
     output.close()
     if not content:
-        colorPrint("当前未安装xcode-select,现在开始安装:", "33")
+        colorPrint(Yellow, "当前未安装xcode-select,现在开始安装:")
         os.system("xcode-select --install")
     elif "Xcode.app/Contents/Developer" not in content.strip():
-        colorPrint("工具会使用到脚本打包项目的功能,需要设置xcode-select对应的位置", "33")
-        colorPrint("请在终端执行以下命令完成设置:", "33")
-        print("sudo xcode-select -s %s" % (content.strip()))
-        colorPrint("因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
-        colorPrint("执行完成后重新运行当前脚本即可")
+        colorPrint(Yellow, "工具会使用到脚本打包项目的功能,需要设置xcode-select对应的位置")
+        colorPrint(Yellow, "请在终端执行以下命令完成设置:")
+        colorPrint(Green, "sudo xcode-select -s %s" % (content.strip()))
+        colorPrint(Red, "因为涉及到sudo,本工具不提供自动执行操作,建议自行了解以上命令后决定")
+        colorPrint(Red, "执行完成后重新运行当前脚本即可")
         os._exit(1)
 
 
@@ -129,7 +135,7 @@ def getPip3List():
     lines = output.readlines()
     output.close()
     if not lines:
-        colorPrint("检测到未安装pip3模块,请重新运行当前脚本")
+        colorPrint(Red, "检测到未安装pip3模块,请重新运行当前脚本")
         os._exit(1)
     return lines
 
@@ -196,19 +202,19 @@ def checkOperateEnv():
     checkXcodeInstalled()
     checkXcodeSelect()
     time = 1
-    colorPrint("Pip提示更新的警告可以忽略", "32")
+    colorPrint(Yellow, "pip更新的提示可忽略")
     while not isAllRequiredLibInstalled():
         if time == 1:
-            colorPrint("首次运行将自动安装工具依赖库", "33")
+            colorPrint(Yellow, "首次运行将自动安装工具依赖库")
         if time <= 3:
-            colorPrint("\n运行环境不符合要求,正在安装依赖[%d]" % (time), "32")
+            colorPrint(Green, "\n运行环境不符合要求,正在安装依赖[%d]" % (time))
             installRequiredLib()
             time += 1
         else:
-            colorPrint("多次安装依赖库失败,请确认网络连接,重启终端重试")
-            colorPrint("或者尝试手动安装依赖,所有依赖库在上面的第26行代码里")
-            colorPrint("\t例如安装biplist,命令如下:")
-            colorPrint("\tpip3 install biplist==1.0.2")
+            colorPrint(Red, "多次安装依赖库失败,请确认网络连接,重启终端重试")
+            colorPrint(Red, "或者尝试手动安装依赖,所有依赖库在上面的第26行代码里")
+            colorPrint(Red, "\t例如安装biplist,命令如下:")
+            colorPrint(Red, "\tpip3 install biplist==1.0.2")
             os._exit(1)
     print("*" * 32 + "运行环境符合要求" + "*" * 32)
 
@@ -222,8 +228,8 @@ os.chdir(parPath)
 try:
     import JustDoIT
 except:
-    colorPrint("解决方案1:\n\t初次完成依赖安装请重启终端再次运行")
-    colorPrint("解决方案2:\n\t输入python3,输入空格,将start.py文件拖到空格后面,输入回车")
+    colorPrint(Red, "解决方案1:\n\t初次完成依赖安装请重启终端再次运行")
+    colorPrint(Red, "解决方案2:\n\t请参考同级目录下的「使用演示.gif」")
 
 if __name__ == "__main__":
     JustDoIT.justDoIT()
